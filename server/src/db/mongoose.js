f05d320a2f297a958952fb1f68287bdc69be3dfe
Samiul islam <41300563+samiulislamakash@users.noreply.github.com>
2020-07-17 14:50:42 +0600

    Update mongoose.js

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb/TaskManager',{
+mongoose.connect('mongodb://127.0.0.1:27017/TaskManager',{
     useCreateIndex:true,
     useNewUrlParser: true,
     useFindAndModify: false,
