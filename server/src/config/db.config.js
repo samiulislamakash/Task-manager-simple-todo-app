51bfb6804696596587924b18b266d825666a0481
Samiul Islam <samiulislamw.w.w@gmail.com>
2021-03-05 21:25:14 +0600

    add atlas database connection :)

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/TaskManager', {
+mongoose.connect('mongodb+srv://TodoDB:TodoDB@cluster0.eykna.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
